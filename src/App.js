4836f9b25ebd79a17094b5d9d3f9428808263e5f
maarcf <marr.cairo@gmail.com>
2021-05-08 12:48:15 -0300

    start project with routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+
+import { BrowserRouter, Route } from "react-router-dom";
 import './App.css';
 
 const App = () => {
   return (
-    <div className="App">
-      <h1>Soy APP</h1>
-    </div>
+    <BrowserRouter>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/movies" component={Movies} />
+      <Route exact path="/movies/:category/page/:pageNumber" component={MoviesCategory} />
+      <Route exact path="/movies/:id/info" component={MovieDetail} />
+      <Route exact path="/series" component={Series} />
+      <Route exact path="/series/:category/page/:pageNumber" component={SeriesCategory} />
+      <Route exact path="/series/:id/info" component={SerieDetail} />
+      <Route exact path="/search/:query/page/:pageNumber" component={Search} />
+      <Route component={PageNotFound} />
+    </BrowserRouter>
   );
 }
 
